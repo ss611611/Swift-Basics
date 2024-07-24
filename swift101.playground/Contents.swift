d8614ff326f7b3f69171c6a67442a3669c8ecdf4
jackie <ss611611@gmail.com>
2024-07-24 11:46:50 +0800

    Initial commit

---
+++ swift101.playground/Contents.swift
@@ -0,0 +1,26 @@
+// Comment
+
+// 建立變數
+
+var number = 1
+print(number)
+print("Hello World")
+
+// 改變變數
+number = 2
+print(number)
+
+// debug (除錯)
+let number2: Int = 2
+print(number2)
+
+// 實作：打招呼
+
+var name = "Jane"
+let location = "Taiwan"
+
+print("Hello", name, "from", location)
+print("Hello \(name) from \(location)")
+
+name = "Amy"
+print("Hello \(name) from \(location)")
